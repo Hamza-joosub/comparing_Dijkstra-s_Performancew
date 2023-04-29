6367ca8a2bc27c9bd2a741f0748853bbbf0fa436
Hamza <125697787+Hamza-joosub@users.noreply.github.com>
2023-04-29 04:33:05 +0200

    update

--- DataSet.java
+++ DataSet.java
@@ -4,15 +4,25 @@ public class DataSet
     String name = "Node";
     Vertice[] arr_V;
     Edge[] arr_E;
-
+    int count = 0;
     public DataSet(int total_num_of_v,int total_num_of_e)
-    {
-        for (int i = 0;i<total_num_of_v-1;i++)
-        {
-            arr_V[i] = new Vertice(name + i);
-        }
-        
-        arr_E = new Edge[total_num_of_e];  
+    {   
+        arr_V = new Vertice[total_num_of_v];
+        arr_E = new Edge[total_num_of_e];
+
     }
-         
+    public Vertice generate()
+    {
+        if(count != total_num_of_v )
+        {
+        arr_V[count] = new Vertice("Node"+count);
+        arr_V[count].addEdge(new Edge(arr_V[count],generate(),2));
+        count++;
+        }
+        else
+        {
+            return arr_V[count];
+        }
+        return arr_V[count];
+    } 
 }
--- Main.java
+++ Main.java
@@ -2,7 +2,9 @@ public class Main {
     
     public static void main(String args[])
     {
-        
+        DataSet data = new DataSet(5,10);
+        data.generate();
+        System.out.println(data.arr_V);
 
 
     }
