c6a785cca536e9bedde7dcc67e9aae90d4796f2f
Hamza <125697787+Hamza-joosub@users.noreply.github.com>
2023-05-03 21:11:00 +0200

    recursive function alteration

--- DataSet.java
+++ DataSet.java
@@ -4,24 +4,26 @@ public class DataSet
     String name = "Node";
     Vertice[] arr_V;
     Edge[] arr_E;
-    int count = 0;
+    int count = -1;
     public DataSet(int total_num_of_v,int total_num_of_e)
     {   
         arr_V = new Vertice[total_num_of_v];
         arr_E = new Edge[total_num_of_e];
-
+        this.total_num_of_v = total_num_of_v;
+        this.total_num_of_e = total_num_of_e;
     }
-    public Vertice generate()
+    public Vertice generateHelper()
     {
-        if(count != total_num_of_v )
+        if(count != total_num_of_v-1 )
         {
-        arr_V[count] = new Vertice("Node"+count);
-        arr_V[count].addEdge(new Edge(arr_V[count],generate(),2));
         count++;
+        arr_V[count] = new Vertice("Node"+count);
+        arr_V[count].addEdge(new Edge(arr_V[count],generateHelper(),2));
+        
         }
         else
         {
-            return arr_V[count];
+
         }
         return arr_V[count];
     } 
