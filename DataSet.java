e7182f979be1c878e06d008255226673768f6b78
Hamza <125697787+Hamza-joosub@users.noreply.github.com>
2023-05-05 15:12:41 +0200

    scrapped recursive method, implemented alternative

--- DataSet.java
+++ DataSet.java
@@ -6,15 +6,47 @@ public class DataSet
     Vertice[] arr_V;
     Edge[] arr_E;
     Random rand = new Random();
-    int random;
+    int random,randomSource,randomDestination;
     public DataSet(int total_num_of_v,int total_num_of_e)
     {   
-        arr_V = new Vertice[total_num_of_v];
+        arr_V = new Vertice[total_num_of_v+1];
         arr_E = new Edge[total_num_of_e];
-        this.total_num_of_v = total_num_of_v;
+        this.total_num_of_v = total_num_of_v+1;
         this.total_num_of_e = total_num_of_e;
     }
     
+    public void generate()
+    {
+        for (int i = 0;i<total_num_of_v;i++) //create all nodes
+        {
+            arr_V[i] = new Vertice("Node" + i);
+        }
+        for (int j = 0;j<=arr_V.length-1;j++) //add at least one edge to each node ignore last node
+        {
+            arr_V[j].addEdge(new Edge(arr_V[j],arr_V[generateRandomNumber(arr_V.length-1,j)],2));
+        }
+        for(int p = 0;p<(total_num_of_e-total_num_of_v+1);p++)//add edges to and from random vertices
+        {
+            randomSource = generateRandomNumber(arr_V.length-1, -1);
+            randomDestination = generateRandomNumber(arr_V.length-1,randomSource);
+            //arr_V[randomSource].edges.
+            arr_V[randomSource].addEdge(new Edge(arr_V[randomSource], arr_V[randomDestination], 2));
+        }
+    }
+
+    public int generateRandomNumber(int range, int exception)
+    {
+        random = rand.nextInt(range);
+        while(random == exception)
+        {
+            random = rand.nextInt(range);
+        }
+        return random;
+    }
+}
+
+
+/* 
     public Vertice generateDirectLinks(int count)
     {
         if(count != total_num_of_v-1)
@@ -36,30 +68,12 @@ public class DataSet
         {   
             Vertice source = arr_V[i];
             Vertice destination = arr_V[i];
-
-
-
-
-
-            /* 
             for (int j = 0; j<rand.nextInt(4);j++)
             {
 
             arr_V[i].addEdge(new Edge(arr_V[i], arr_V[generateRandomNumber(total_num_of_v,i)], 2));
             }
-            */
+
         }
     }
-
-
-
-    public int generateRandomNumber(int range, int exception)
-    {
-        random = rand.nextInt(range);
-        while(random == exception)
-        {
-            random = rand.nextInt(range);
-        }
-        return random;
-    }
-}
+    */
--- Main.java
+++ Main.java
@@ -2,15 +2,16 @@ public class Main {
     
     public static void main(String args[])
     {
-        DataSet data = new DataSet(5+1,10);
-        data.generateDirectLinks(-1);
-        data.generateCrossLinks();
+        DataSet data = new DataSet(5,10);
+        //data.generateDirectLinks(-1);
+        //data.generateCrossLinks();
+        data.generate();
         System.out.print("Nodes:"+"\n");
-        for(int i=0;i<data.total_num_of_v;i++)
+        for(int i=0;i<data.total_num_of_v-1;i++)
         {
             System.out.println(data.arr_V[i].VtoString());
         }
-        for(int i=0;i<data.total_num_of_v;i++)
+        for(int i=0;i<data.total_num_of_v-1;i++)
         {
             System.out.println(data.arr_V[i].EdgetoString());
         }
